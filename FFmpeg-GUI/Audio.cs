b092ae5e476d9172220413521c0e407877714e38
Kadir Burak Gündüz <kburakgndz@gmail.com>
2022-01-07 16:58:59 +0300

    Create Audio.cs

---
+++ FFmpeg-GUI/Audio.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace FFmpeg_GUI
+{
+    class Audio : IMedia<string>, IAudio
+    {
+        private Audio() { }
+        private static Audio? _instance = null;
+        public static Audio Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Audio();
+
+                }
+                return _instance;
+            }
+        }
+
+        public List<string> Extentions { get; } = new List<string>()
+        {
+            "MP3"
+        };
+        public double Volume { get; set; } = 100;
+        public string Name { get; set; }
+        public string Size { get; set; }
+
+        public string MakeCommandString()
+        {
+            StringBuilder command = new StringBuilder();
+
+            string volume = Math.Round((Volume / 100), 1).ToString().Replace(",", ".");
+
+            command.Append($" -filter:a \"volume = {volume}\" ");
+
+            return command.ToString();
+        }
+    }
+}
